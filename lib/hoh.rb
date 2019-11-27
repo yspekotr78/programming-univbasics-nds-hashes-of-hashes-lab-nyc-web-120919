16d0938314bfed4942b744d2ea92acce94c35ec2
Yury Spektor <yspektor78@gmail.com>
2019-11-27 23:45:11 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -54,6 +54,7 @@ def naming_system
                   :label => "Genus",
                   :sub_category => {
                       :label => "Species",
+                      :sub_category => nil
                   }
               }
           }
